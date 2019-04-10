36e5a7789313c0c95cdc27206ee7416518f8e0a9
Xuuuuu <18382705187@163.com>
2019-04-10 18:20:36 +0800

    AOP Demo

--- aopdemo/src/test/java/com/xuuuuu/aopdemo/AopTest.java
+++ aopdemo/src/test/java/com/xuuuuu/aopdemo/AopTest.java
@@ -3,7 +3,7 @@ package com.xuuuuu.aopdemo;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+ 
 public class AopTest {
   @Test
   public void testService() {
