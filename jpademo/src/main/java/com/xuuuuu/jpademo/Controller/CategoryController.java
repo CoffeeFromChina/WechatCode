d5f842373100a3359a913ab053bfb08b0e8a7bb7
Xuuuuucong <18382705187@163.com>
2019-07-04 09:37:33 +0800

    JPA

--- jpademo/src/main/java/com/xuuuuu/jpademo/Controller/CategoryController.java
+++ jpademo/src/main/java/com/xuuuuu/jpademo/Controller/CategoryController.java
@@ -1,32 +1,40 @@
 package com.xuuuuu.jpademo.Controller;
 
-import com.xuuuuu.jpademo.Service.CategoryService;
+import com.xuuuuu.jpademo.dao.CategoryDao;
 import com.xuuuuu.jpademo.pojo.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
 public class CategoryController {
-
 	@Autowired
-	CategoryService categoryService;
+	CategoryDao categoryDao;
 
-	@GetMapping("/")
-	public @ResponseBody
-	List<Category> index() {
-		//获取所有数据
-		List<Category> categoryList = categoryService.getAll();
+	@RequestMapping(value = {"/", "index"})
+	public String index(Model model,
+	                     @RequestParam(defaultValue = "0",
+			                     required = true,
+			                     value = "pageNo") Integer pageNo){
 
-		return categoryList;
+		Integer pageSize = 2;//每页显示记录数。pageNo是第几页。
+		Pageable pageable = new PageRequest(pageNo, pageSize);
+		Page<Category> page = categoryDao.findAll(pageable);
+		model.addAttribute("page", page);
+		return "index";
 	}
 
 	@PostMapping("/add")
@@ -34,38 +42,29 @@ public class CategoryController {
 	List<Category> add(){
 
 		//获取所有记录数量
-		int sum = categoryService.getCount();
-
-		//实例化Category对象
+		Long sum = categoryDao.count();
+		//实例化Category对象。使用的时Lombok提供的构造方法。
 		Category category = Category.builder()
 									.name("category" + (sum+1))
 									.build();
-
-		//保持Category对象
-		categoryService.save(category);
+		//保存Category对象
+		categoryDao.save(category);
 
 		//获取所有数据
-		List<Category> categoryList = categoryService.getAll();
-
+		List<Category> categoryList = categoryDao.findAll();
 		return categoryList;
 	}
 
 	@DeleteMapping("/del/{id}")
 	public @ResponseBody
 	List<Category> del(@PathVariable("id") Long id){
-
-		//获取Id
-		Long Id = id;
-
 		//根据Id删除Category对象
-		categoryService.delete(id);
+		categoryDao.deleteById(id);
 
 		//获取所有数据
-		List<Category> categoryList = categoryService.getAll();
-
+		List<Category> categoryList = categoryDao.findAll();
 		return categoryList;
 	}
-
 
 	//JPA是通过save()来进行插入和更新操作，判断依据是主键是否被赋值
 	//如果主键被赋值，那么会查找主键数据，如果存在则执行更新操作，否则则执行插入操作。
@@ -75,13 +74,15 @@ public class CategoryController {
 	public @ResponseBody
 	List<Category> update(@PathVariable("id") Long id,
 	                      @PathVariable("name") String updateName){
-
-		categoryService.update(id, updateName);
+		//根据Id查找Category
+		Category category = categoryDao.findCategoryById(id);
+		//重新设置修改的字段信息
+		category.setName(updateName);
+		//更新数据
+		categoryDao.save(category);
 
 		//获取所有数据
-		List<Category> categoryList = categoryService.getAll();
-
+		List<Category> categoryList = categoryDao.findAll();
 		return categoryList;
 	}
-
 }
--- jpademo/src/main/java/com/xuuuuu/jpademo/dao/CategoryDao.java
+++ jpademo/src/main/java/com/xuuuuu/jpademo/dao/CategoryDao.java
@@ -2,6 +2,8 @@ package com.xuuuuu.jpademo.dao;
 
 import com.xuuuuu.jpademo.pojo.Category;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,6 +15,8 @@ public interface CategoryDao extends JpaRepository<Category, Long> {
 	//命名规则参考文章
 	//https://blog.csdn.net/sbin456/article/details/53304148
 
+	//自定义的Pageable分页方法。
+	Page<Category> findAll(Pageable pageable);
 
 	//自定义的查找方法。只需根据JPA的命名规则创建即可
 	//“nativeQuery=true”来表示是原生SQL
